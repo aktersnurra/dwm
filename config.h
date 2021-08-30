4fb26cd4a45cc90042cd6cb06077b51b20fa83c4
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2021-08-30 21:03:46 +0200

    Updates

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* Constants */
-#define TERMINAL "alacritty"
-#define TERMCLASS "Alacritty"
+#define TERMINAL "st"
+#define TERMCLASS "St"
 
 /* appearance */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
