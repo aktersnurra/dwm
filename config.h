a61ea1f0bce6e01284320de4400fd89ee6e3de47
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2023-05-15 23:39:17 +0200

    Add floatterm to rules

--- config.h
+++ config.h
@@ -50,11 +50,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ TERMCLASS, NULL,     NULL,           0,         0,          1,          0,         -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,          1,         -1 },
-  { TERMCLASS, "spterm", NULL,       	   SPTAG(0),  1,          1,          0,         -1 },
-	{ TERMCLASS, "spcalc", NULL,       	   SPTAG(1),  1,          1,          0,         -1 },
+	/* class     instance     title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ TERMCLASS, NULL,        NULL,           0,         0,          1,          0,         -1 },
+	{ NULL,      NULL,        "Event Tester", 0,         0,          0,          1,         -1 },
+  { TERMCLASS, "floatterm", NULL,       	  0,         1,          1,          0,         -1 },
+  { TERMCLASS, "spterm",    NULL,       	  SPTAG(0),  1,          1,          0,         -1 },
+	{ TERMCLASS, "spcalc",    NULL,       	  SPTAG(1),  1,          1,          0,         -1 },
 };
 
 /* layout(s) */
