0d1140ef20adb2a7071466cef5aeb87556496879
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2024-10-25 00:41:58 +0200

    Remove const in config
    This fixes a compile bug that complained about fonts being const char *
    instead of char *

--- config.h
+++ config.h
@@ -8,16 +8,16 @@
 /* appearance */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;  /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 20; /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10; /* vert inner gap between windows */
-static const unsigned int gappoh    = 10; /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30; /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;  /* 1 means no outer gap when there is only one window */
+static int swallowfloating    = 0;  /* 1 means swallow floating windows by default */
+static unsigned int gappih    = 20; /* horiz inner gap between windows */
+static unsigned int gappiv    = 10; /* vert inner gap between windows */
+static unsigned int gappoh    = 10; /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 30; /* vert outer gap between windows and screen edge */
+static int smartgaps          = 0;  /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=8", "Symbols Nerd Font:style=Regular:pixelsize=12:antialias=true:autohint=true" };
-static const char dmenufont[]       = "monospace:size=8";
+static char *fonts[]          = { "monospace:size=8", "Symbols Nerd Font:style=Regular:pixelsize=12:antialias=true:autohint=true" };
+static char dmenufont[]       = "monospace:size=8";
 static char normbgcolor[] =     "#000000";
 static char normbordercolor[] = "#000000";
 static char normfgcolor[] =     "#eeeeee";
